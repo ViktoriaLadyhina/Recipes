72082a61b2f0908081786492f3ee208498036874
Вика <vika.ladyhina@gmail.com>
2025-09-01 10:28:13 +0200

    correct SCSS import paths for Vercel deployment

--- src/components/categoryCard/CategoryCard.jsx
+++ src/components/categoryCard/CategoryCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router";
-import "./CategoryCard.scss";
+import "./сategoryCard.scss";
 
 const CategoryCard = ({ category }) => {
     return (
